44ed054713e2f933a5d6855066ef07da1c490dfa
Varsha Jain <csvarshajain@gmail.com>
2021-11-10 22:50:28 -0500

    added post routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+// reverse association 
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
 module.exports = { User, Post };
